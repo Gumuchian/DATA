d310af06f7375afbbd246c101acfe86555fe7741
Paul <Paul@10.120.1.95>
2016-11-08 10:44:21 +0100

    MAJ

--- data/src/data/data.java
+++ data/src/data/data.java
@@ -1,10 +1,27 @@
 package data;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class data {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try{
+			InputStream file=new FileInputStream("data_science"); 
+			InputStreamReader fich=new InputStreamReader(file);
+			BufferedReader br=new BufferedReader(fich);
+			String ligne=br.readLine();
+			while (ligne!=null){
+			}
+			fich.close();
+			file.close();
+			br.close(); 
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
 	}
 
 }
