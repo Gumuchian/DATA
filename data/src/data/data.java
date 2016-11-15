cb26a0aa4defb6745101bea70263b698a53ec26b
paul <paul@10.136.0.99>
2016-11-15 13:56:06 +0100

    MAJ I et Q

--- data/src/data/data.java
+++ data/src/data/data.java
@@ -14,24 +14,34 @@ public class data {
 			Path path=Paths.get("data_science.dat");
 			byte[] data=Files.readAllBytes(path);
 			short t[][]=new short[40][(data.length-4)/184];
-			FileWriter fw = new FileWriter ("data.txt");
+			short t1[][]=new short[40][(data.length-4)/184];
+			FileWriter fw = new FileWriter ("dataI.txt");
+			FileWriter fw1 = new FileWriter ("dataQ.txt");
 			BufferedWriter bw = new BufferedWriter (fw);
+			BufferedWriter bw1 = new BufferedWriter (fw1);
 			int i,j;
 			PrintWriter fichierSortie = new PrintWriter (bw);
+			PrintWriter fichierSortie1 = new PrintWriter (bw1);
 			for (i=0;i<(data.length-4)/184;i++){
 				for (j=0;j<40;j++){
 					t[j][i]=(short) ((data[i*164+8+j*4]<<8)+(data[i*164+8+1+j*4]));
+					t1[j][i]=(short) ((data[i*164+12+j*4]<<8)+(data[i*164+12+1+j*4]));
 					if (j!=39){
 						fichierSortie.print(t[j][i]+"\t");
+						fichierSortie1.print(t1[j][i]+"\t");
 					}
 					else{
 						fichierSortie.println(t[j][i]);
+						fichierSortie1.println(t1[j][i]);
 					}
 				}
 			}
 			fichierSortie.close();
 			fw.close();
 			bw.close();
+			fichierSortie1.close();
+			fw1.close();
+			bw1.close();
 		}
 		catch (Exception e){
 			System.out.println(e.toString());
